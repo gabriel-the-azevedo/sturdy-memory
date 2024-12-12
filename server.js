c1a0727ad9b9421945b09cc76b9f2cc9fd722d99
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2024-12-12 16:05:19 -0300

    feat: add click me button

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 const port = 3000
 app.use(express.static("public"))
 
+app.post("/clicked", (_, res) => {
+    res.send("<h2>Clicked<h2>")
+})
+
 app.listen(port, function () {
     console.log(`Server is running at http://localhost:${port}`);
 });
