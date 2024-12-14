c5aefef6bba85eb573f68262154ae6a8fc9906d0
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2024-12-14 12:23:14 -0300

    feat: simple todo app

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import express from "express"
 
 const app = express()
 const port = 3000
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
-app.post("/clicked", (_, res) => {
-    res.send("<h2>Clicked<h2>")
+app.post("/add-item", (req, res) => {
+    const { item } = req.body
+    res.send(`<li hx-delete="/delete-item" hx-swap="outerHTML">${item}</li>`)
+})
+
+app.delete("/delete-item", (_, res) => {
+    res.status(200).send()
 })
 
 app.listen(port, function () {
